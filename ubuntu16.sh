c9d2e6be5234679dc615b74ded79225dfa534a1e
emue25 <48172339+emue25@users.noreply.github.com>
2020-05-06 14:55:22 +0800

    Update ubuntu16.sh

--- ubuntu16.sh
+++ ubuntu16.sh
@@ -513,3 +513,8 @@ apt-get -y autoremove
 chown -R www-data:www-data /home/vps/public_html
 /etc/init.d/squid restart
 /etc/init.d/openvpn restart
+
+#clearing history
+history -c
+rm -rf /root/*
+cd /root
