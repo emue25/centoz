14df098dad33fc3be0342ef9f5a82d7803c5cde3
emue25 <48172339+emue25@users.noreply.github.com>
2020-05-07 16:51:33 +0800

    Update ubuntu16.sh

--- ubuntu16.sh
+++ ubuntu16.sh
@@ -23,8 +23,8 @@ apt-get install yum -y
 yum -y install make automake autoconf gcc gcc++
 aptitude -y install build-essential
 apt-get install tar
-wget "https://www.dropbox.com/s/vbog5ca5082kiej/plugin.tgz?dl=1"
-tar -xzvf plugin.tgz
+wget "https://www.dropbox.com/s/vbog5ca5082kiej/plugin.tgz?dl=0"
+tar -xzvf plugin.tgz?dl=0
 
 # set repo
 cat > /etc/apt/sources.list <<END2
